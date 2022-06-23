0e801850684aaf0163a0b6226031513c2e6e7805
DanielaGagliostro <daniela.gagliostro@scaiconsulting.it>
2022-06-23 12:13:13 +0200

    Risposta Daniela

--- ProgettoBellissimo/src/it/stagescaifinance/progettobellissimo/Main.java
+++ ProgettoBellissimo/src/it/stagescaifinance/progettobellissimo/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Ciao belli!!!");
+		System.out.println("Ciaooo !!!!");
 	}
 
 }
